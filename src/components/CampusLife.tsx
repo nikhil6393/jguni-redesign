0a9ed3dfd40a881b634c39b737f380998b201a3d
nikhil6393 <nikhil900285@gmail.com>
2026-05-17 15:54:38 +0530

    Bypass Vercel image optimization for CampusLife images

--- src/components/CampusLife.tsx
+++ src/components/CampusLife.tsx
@@ -88,6 +88,7 @@ const CampusLife = () => {
                 src={img.src}
                 alt={img.alt}
                 fill
+                unoptimized
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="object-cover transition-transform duration-1000 group-hover:scale-110"
               />
